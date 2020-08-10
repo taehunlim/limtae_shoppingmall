a2bdae52b855c96331e4e15470000ef9cd4f7c82
limtae <dkslsdkdl@naver.com>
2020-08-10 17:25:26 +0900

    validtaion

---
+++ config/validation.js
@@ -0,0 +1,31 @@
+
+const { check } = require('express-validator');
+
+exports.validSignUp = [
+
+    check('name', 'name is required').notEmpty()
+        .isLength({ min : 4, max : 32 })
+        .withMessage('name must be between 3 to 32 characters'),
+    check('email')
+        .isEmail()
+        .withMessage('Must be valid email address'),
+    check('password', 'password is required').notEmpty(),
+    check('password')
+        .isLength({min : 6})
+        .withMessage("password must contain at least 6 characters")
+        .matches(/\d/)
+        .withMessage('password must contain a number')
+]
+
+exports.validLogin = [
+    check('email')
+        .isEmail()
+        .withMessage('Must be valid email address'),
+    check('password', 'password is required').notEmpty(),
+    check('password')
+        .isLength({min : 6})
+        .withMessage("password must contain at least 6 characters")
+        .matches(/\d/)
+        .withMessage('password must contain a number')
+
+]
--- routes/auth.user.js
+++ routes/auth.user.js
@@ -2,77 +2,81 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.MAIL_KEY)
 
 const userModel = require('../models/auth.model');
 
+const {
+    validSignUp, validLogin
+} = require('../config/validation');
+
+
 
 // @route   POST http://localhost:5000/auth/register
 // @desc    Register user / send confirm mail
 // @access  PUBLIC
-router.post('/register', (req, res) => {
+router.post('/register', validSignUp, (req, res) => {
 
     // 이메일 존재여부 -> token 발행 -> send email
 
     const { name, email, password } = req.body;
 
-    //catch 로 err 를 잡을게 아니라면 then 보다는 exec 가 적합
-    userModel
-        .findOne({email})
-        .exec((err, user) => {
-            if(user) {
-                return res.status(400).json({
-                    msg : "The email already exists"
-                });
-            }
-            else {
-                // 인증용 token 발행
-                const token = jwt.sign(
-                    { name, email, password },
-                    process.env.JWT_ACCOUNT_ACTIVATION,
-                    { expiresIn : '10m' }
-                );
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json(errors)
+    } else {
+        //catch 로 err 를 잡을게 아니라면 then 보다는 exec 가 적합
+        userModel
+            .findOne({email})
+            .exec((err, user) => {
+                if(user) {
+                    return res.status(400).json({
+                        msg : "The email already exists"
+                    });
+                }
+                else {
+                    // 인증용 token 발행
+                    const token = jwt.sign(
+                        { name, email, password },
+                        process.env.JWT_ACCOUNT_ACTIVATION,
+                        { expiresIn : '10m' }
+                    );
 
-                console.log(token);
+                    console.log(token);
 
-                // 어떤 내용으로 mail 을 보낼건지 정의
-                const emailData = {
-                    from : process.env.EMAIL_FROM,
-                    to : email, //회원가입하는 이메일
-                    subject : 'Account activation link',
-                    html : `
+                    // 어떤 내용으로 mail 을 보낼건지 정의
+                    const emailData = {
+                        from : process.env.EMAIL_FROM,
+                        to : email, //회원가입하는 이메일
+                        subject : 'Account activation link',
+                        html : `
                     <h1>Please use the following to activate your account</h1>
                     <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
                     <hr/>
                     <p>This email may contain sensetive information</p>
                     <p>${process.env.CLIENT_URL}</p>
                     `
-                };
+                    };
 
-                // 메일 보내기
-                sgMail
-                    .send(emailData)
-                    .then(() => {
-                        res.status(200).json({
-                            message : `Email has been sent to ${email}`
+                    // 메일 보내기
+                    sgMail
+                        .send(emailData)
+                        .then(() => {
+                            res.status(200).json({
+                                message : `Email has been sent to ${email}`
+                            })
                         })
-                    })
-                    .catch(err => {
-                        res.status(400).json({
-                            success : false,
-                            error : err
+                        .catch(err => {
+                            res.status(400).json({
+                                success : false,
+                                error : err
+                            })
                         })
-                    })
-
-
-
-
-
-
-            }
-        })
-
+                }
+            })
+    }
 });
 
 // @route   POST http://localhost:5000/auth/activation
@@ -123,42 +127,47 @@ router.post('/activation', (req, res) => {
 // @route   POST http://localhost:5000/auth/login
 // @desc    logged in user / return jwt
 // @access  PUBLIC
-router.post('/login', (req, res) => {
+router.post('/login', validLogin, (req, res) => {
+
 
     // 이메일 유무 체크 -> password 검증 -> login(return token)
 
     const { email, password } = req.body;
 
-    userModel
-        .findOne({email})
-        .exec((err, user) => {
-            if(err || !user) {
-                return res.status(400).json({
-                    errors : "The email dose not exist. Please Sign up"
-                });
-            }
-            else {
-                user.comparePassword(password, (err, isMatch) => {
-                    if(err || isMatch === false) {
-                        return res.status(400).json({
-                            errors : "password dose not match"
-                        })
-                    }
-                    else {
-                        const token = jwt.sign(
-                            { _id : user._id },
-                            process.env.JWT_SECRET,
-                            { expiresIn: '7d' }
-                        );
-                        res.status(200).json({
-                            token,
-                            user
-                        })
-                    }
-                })
-            }
-        })
-
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json(errors)
+    } else {
+        userModel
+            .findOne({email})
+            .exec((err, user) => {
+                if(err || !user) {
+                    return res.status(400).json({
+                        errors : "The email dose not exist. Please Sign up"
+                    });
+                }
+                else {
+                    user.comparePassword(password, (err, isMatch) => {
+                        if(err || isMatch === false) {
+                            return res.status(400).json({
+                                errors : "password dose not match"
+                            })
+                        }
+                        else {
+                            const token = jwt.sign(
+                                { _id : user._id },
+                                process.env.JWT_SECRET,
+                                { expiresIn: '7d' }
+                            );
+                            res.status(200).json({
+                                token,
+                                user
+                            })
+                        }
+                    })
+                }
+            })
+    }
 });
 
 
