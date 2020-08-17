1ba76e9dcb0e0980f5d65b4f5d4394575b737ee2
limtae <dkslsdkdl@naver.com>
2020-08-17 14:22:26 +0900

    Register screen

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,18 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import {ToastContainer} from 'react-toastify'
 import { Link, Redirect } from 'react-router-dom';
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name : '',
+        email : '',
+        password1 : '',
+        password2 : '',
+        textChange : 'Sign up'
+    })
+
+    const { name, email, password1, password2, textChange } = formData;
+
+    const handleChange = text => e => {}
+
+    const handleSubmit = e => {}
+
     return (
         <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
             <ToastContainer/>
-            <div>
-                <div>
-                    <div>
+            <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+                <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
+                    <div className='mt-12 flex flex-col items-center'>
                         <h1 className='text-2xl xl:text-3xl font-extrabold'>
                             Sign up for here
                         </h1>
+
+                        <form
+                            className='w-full flex-1 mt-8 text-indigo-500'
+                            onSubmit={handleSubmit}
+                        >
+                            <div className='mx-auth max-w-xs relative'>
+                                <input
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                    type='text'
+                                    placeholder='Name'
+                                    onChange={handleChange('name')}
+                                    value={name}
+                                />
+                                <input
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    type='email'
+                                    placeholder='email'
+                                    onChange={handleChange('email')}
+                                    value={email}
+                                />
+                                <input
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    type='password'
+                                    placeholder='password'
+                                    onChange={handleChange('password1')}
+                                    value={password1}
+                                />
+                                <input
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    type='password'
+                                    placeholder='confirm password'
+                                    onChange={handleChange('password2')}
+                                    value={password2}
+                                />
+                                <button
+                                    type='submit'
+                                    className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                                >
+                                    <i className='fas fa-user-plus fa 1x w-6 -ml-2'/>
+                                    <span className="ml-3">{textChange}</span>
+                                </button>
+                                <div className='my-12 border-b text-center'>
+                                    <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
+                                        Or sign with email or social login
+                                    </div>
+                                </div>
+                                <div className='flex flex-col items-center'>
+                                    <a
+                                        className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                                        href='/login'
+                                        target='_self'
+                                    >
+                                        <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
+                                        <span className='ml-4'>Sign In</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
