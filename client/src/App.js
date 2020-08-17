0645999fa12a32131b5e1f82206382323cd16431
limtae <dkslsdkdl@naver.com>
2020-08-17 13:49:14 +0900

    client main

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>work!</h1>
-    </div>
+      <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+          <ToastContainer />
+          <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+              <div className="lg:w-1/2 xl:w-8/12 p-6 sm:p-12">
+                  <div className="mt-12 flex flex-col items-center">
+                      <h1 className="text-2xl xl:text-2xl font-extrabold text-center">
+                          Ultimate Auth with Email & social with different roles,
+                          email verification & forget password
+                      </h1>
+                      <div className='w-full flex-1 mt-8 text-indigo-500'>
+                          <div className='my-12 border-b text-center'>
+                              <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
+                                  Features
+                              </div>
+                          </div>
+                          <div className="mx-auto max-w-xs relative">
+                              <a
+                                  to='/login'
+                                  className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                              >
+                                  <i className='fas fa-sign-in-alt  w-6  -ml-2' />
+                                  <span className='ml-3'>Sign In</span>
+                              </a>
+                          </div>
+                          <div className="mx-auto max-w-xs relative">
+                              <a
+                                  to='/register'
+                                  className='mt-5 tracking-wide font-semibold bg-gray-500 text-gray-100 w-full py-4 rounded-lg hover:bg-gray-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                              >
+                                  <i className='fas fa-user-plus  w-6  -ml-2' />
+                                  <span className='ml-3'>Register</span>
+                              </a>
+                          </div>
+                          <div className="mx-auto max-w-xs relative">
+                              <a
+                                  to='/private'
+                                  className='mt-5 tracking-wide font-semibold bg-orange-500 text-gray-100 w-full py-4 rounded-lg hover:bg-orange-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                              >
+                                  <i className='fas fa-sign-in-alt  w-6  -ml-2' />
+                                  <span className='ml-3'>Profile dashboard</span>
+                              </a>
+                          </div>
+                          <div className="mx-auto max-w-xs relative">
+                              <a
+                                  to='/admin'
+                                  className='mt-5 tracking-wide font-semibold bg-green-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                              >
+                                  <i className='fas fa-sign-in-alt  w-6  -ml-2' />
+                                  <span className='ml-3'>Admin dashboard</span>
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
